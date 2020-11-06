0f33d80425eb1be5ea97594ff082b5148045202d
63070109 <73678996+63070109@users.noreply.github.com>
2020-11-06 10:57:56 +0700

    Add files via upload

---
+++ pic109.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-lg-6 col-xl-6">
+                <img src="pic109/t2.jpg" style="height: 30%">
+            </div>
+            <div class="col-sm-3 col-lg-3 col-xl-3">
+                <img src="pic109/t1.jpg">
+                <img src="pic109/t4.jpg" style="height: 10%;">
+                <img src="pic109/t3.jpg" style="height: 20%;">
+            </div>
+            <div class="col-sm-3 col-lg-3 col-xl-3">
+                <img src="pic109/t5.jpg" style="height: 30%;">
+                <img src="pic109/t6.jpg" style="height: 30%;">
+            </div>
+                
+</body>
